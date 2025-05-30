a9f9e2dc6a0f88177baca13196ec25bce94e654e
Dzanija Ivan <ivan.dzanija@tutamail.com>
2025-05-30 11:57:35 +0200

    auto-commit

--- utility/bit_twiddling.hpp
+++ utility/bit_twiddling.hpp
@@ -8,7 +8,7 @@ inline bool same_sign(int a, int b) { return ((a ^ b) >= 0); }
 
 inline bool is_power_of_two(int n) { return (n > 0) && ((n & (n - 1)) == 0); }
 
-inline size_t most_significant_bit(int n) {
+inline unsigned int most_significant_bit(int n) {
     double d = static_cast<double>(n);
     return (n == 0) ? 0 : std::ilogb(d) + 1;
 }
